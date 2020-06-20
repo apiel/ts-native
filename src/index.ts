9fbc0150b83c5ab6f168123b3b863d720a932e8f
Alexandre Piel <alexandre.piel@gmail.com>
2020-06-20 23:19:34 +0200

    wasm

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { argv, cwd } from 'process';
 import { join } from 'path';
-import { shell } from './shell';
 import { log } from 'logol';
+import { readdirSync, copyFileSync } from 'fs';
+
+import { shell } from './shell';
+
+const [, , file] = argv;
+const entryPoint = join(cwd(), file);
+log('entryPoint:', entryPoint);
+
+const buildFolder = join(cwd(), 'build');
+const tmpFolder = join(buildFolder, 'tmp');
+const wasmFile = join(tmpFolder, 'lib.wasm');
+const wasmFolder = join(__dirname, '..', 'wasm');
+
+start();
 
 async function start() {
-    const [, , file] = argv;
-    const entryPoint = join(cwd(), file);
-    log('entryPoint:', entryPoint);
-
-    const buildFolder = join(cwd(), 'build');
-    const wasmFile = join(buildFolder, 'tmp', 'lib.wasm');
     await shell(
         'asc',
         `${entryPoint} -b ${wasmFile} --sourceMap --optimize`.split(' '),
     );
+    copyWasm();
 }
 
-start();
+function copyWasm() {
+    const files = readdirSync(wasmFolder);
+    files.forEach(file => {
+        copyFileSync(join(wasmFolder, file), join(tmpFolder, file));
+    });
+}
