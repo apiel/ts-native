506297cbfd70e70b8bb6438aedcd94eaf637c22d
Alexandre Piel <alexandre.piel@gmail.com>
2020-06-23 17:56:02 +0200

    mem

--- core/mem.c
+++ core/mem.c
@@ -5,3 +5,15 @@ wasm_rt_memory_t *mem;
 void init_mem() {
   mem = WASM_RT_ADD_PREFIX(Z_memory);
 }
+
+u32 mem_alloc(u32 size, u32 type) {
+  return WASM_RT_ADD_PREFIX(Z___allocZ_iii)(size, type);
+}
+
+u32 mem_retain(u32 ptr) {
+  return WASM_RT_ADD_PREFIX(Z___retainZ_ii)(ptr);
+}
+
+void mem_release(u32 ptr) {
+  return WASM_RT_ADD_PREFIX(Z___releaseZ_vi)(ptr);
+}
--- core/mem.h
+++ core/mem.h
@@ -6,5 +6,8 @@
 wasm_rt_memory_t *mem;
 
 void init_mem();
+u32 mem_alloc(u32 size, u32 type);
+u32 mem_retain(u32 ptr);
+void mem_release(u32 ptr);
 
 #endif
